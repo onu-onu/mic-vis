3925c8a84cd05590c2e38dc0fb0c69d00535115e
tomoya-onuki <tomoya.onuki28@gmail.com>
2024-04-08 11:40:15 +0900

    縦方向にレイアウトを変更。グラフ統合

--- src/Chart.ts
+++ src/Chart.ts
@@ -1,7 +1,7 @@
 export class Chart {
     private ctx: CanvasRenderingContext2D;
-    private width = 600;
-    private height = 200;
+    private width = 200;
+    private height = 600;
 
     constructor() {
         const cvs: HTMLCanvasElement = <HTMLCanvasElement>document.querySelector('#cvs');
@@ -24,23 +24,23 @@ export class Chart {
 
         this.ctx.clearRect(0, 0, this.width, this.height);
 
-        this.ctx.fillStyle = '#ddd';
-        this.drawSpectrum(freqList);
+        
+        this.drawSpectrum(freqList, volume);
 
         // volume barの描画
         this.ctx.save();
-        this.ctx.translate(0, 80);
-        this.ctx.fillStyle = '#444';
-        this.drawVolumeBar(volumeMax);
-        this.ctx.fillStyle = '#ddd';
-        this.drawVolumeBar(volume);
+        // this.ctx.translate(0, 80);
+        // this.ctx.fillStyle = '#444';
+        // this.drawVolumeBar(volumeMax);
+        // this.ctx.fillStyle = '#ddd';
+        // this.drawVolumeBar(volume);
 
         this.ctx.restore();
     }
 
     private normalization(dataArray: Uint8Array, bufferLength: number) {
         const freqList: number[] = [];
-        const d = 10;
+        const d = 20;
         for (let i = 0; i < dataArray.length; i += d) {
             let tmp = 0;
             for (let j = i; j < i + d; j++) {
@@ -54,43 +54,37 @@ export class Chart {
     private getVolume(dataArray: Uint8Array, bufferLength: number) {
         // すべての周波数成分の平均を計算
         const sum = dataArray.reduce((acc, val) => acc + val, 0);
-        return sum / bufferLength / 255 * 30;
+        return sum / bufferLength / 255 ;
     }
 
 
-    private drawVolumeBar(volume: number) {
-        let y: number = 0;
-        let w: number = 18;
-        let h: number = 30;
-        let margin = { top: 0, right: 2, bottom: 0, left: 0 };
+    private drawSpectrum(freqList: number[], volume: number) {
+        let maxW: number = 150 * window.devicePixelRatio;
+        let h: number = 3 * window.devicePixelRatio;
+        let margin = { top: 0, right: 2, bottom: 2, left: 2 };
 
-        for (let i = 0; i < volume; i++) {
-            let x: number = i * (w + margin.right);
-            this.ctx.beginPath();
-            this.ctx.roundRect(x, y, w, h, [2]);
-            this.ctx.fill();
-        }
-    }
+        let stdVol: number = volume * freqList.length;
+        freqList.forEach((val, i) => {
+            if (i <= stdVol) {
+                this.ctx.fillStyle = '#ddd';
+            } else {
+                this.ctx.fillStyle = '#444';
+            }
 
-    private drawSpectrum(dataArray: number[]) {
-        let maxH: number = 60;
-        let w: number = 10;
-        let margin = { top: 0, right: 2, bottom: 2, left: 0 };
-
-        dataArray.forEach((val, i) => {
-            let w0: number = w;
-            let h0: number = val / 255 * maxH;
-            let x0: number = i * (w + margin.right);
-            let y0: number = maxH - h0;
+            let w0: number = val / 255 * maxW;
+            let h0: number = h;
+            let x0: number = h + margin.left;
+            let y0: number = i * (h + margin.bottom);
 
             this.ctx.beginPath();
             this.ctx.roundRect(x0, y0, w0, h0, [1]);
+            this.ctx.roundRect(x0, y0, w0, h0, [1]);
             this.ctx.fill();
 
-            let w1: number = w;
-            let h1: number = w;
-            let x1: number = x0;
-            let y1: number = maxH + margin.bottom;
+            let w1: number = h;
+            let h1: number = h;
+            let x1: number = 0;
+            let y1: number = y0;
 
             this.ctx.beginPath();
             this.ctx.roundRect(x1, y1, w1, h1, [1]);
